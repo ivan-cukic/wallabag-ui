f2573cee651914a27ec4bcfb3e62c27a80d7fd68
Ivan Čukić <ivan.cukic@kde.org>
2016-08-27 18:17:01 +0200

    Added a server-side script to return JSON with a tag list

---
+++ server/tags.php
@@ -0,0 +1,51 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+include('config.php');
+
+$db = new SQLite3($sqlite_database_file, SQLITE3_OPEN_READONLY);
+
+$query = "
+        select
+            tag.id,
+            tag.label,
+            tag.slug
+        from
+            wallabag_tag as tag
+    ";
+
+$results = $db->query($query);
+
+$is_first = true;
+
+?>
+[
+<?php while ($row = $results->fetchArray()) {
+    $id       = $row['id'];
+    $label    = $row['label'];
+    $slug     = $row['slug'];
+
+    $count    = $db->query("select count() from wallabag_entry_tag where tag_id = '$id'");
+    $count    = floatval($count->fetchArray()[0]);
+
+    if ($count == 0) continue;
+
+    $level = round(log($count));
+
+    if ($level > 4) $level = 4;
+
+    if ($is_first) {
+        $is_first = false;
+    } else {
+        echo ",";
+    }
+?>
+    {
+        "slug" : "<?=$slug ?>",
+        "title" : "<?=$label ?>",
+        "post_count" : <?=$count ?>
+    }
+<?php } ?>
+]
+
